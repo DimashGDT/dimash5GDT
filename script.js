2d55df4de7220339aea1b10e34ccc6683cd619e6
DimashGDT <112397065+DimashGDT@users.noreply.github.com>
2023-04-16 15:56:52 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function MyFunction() {
+
+
+
+    var name = prompt("Напишите отзыв ")
+    if (name.length > 18){
+        alert("Спасибо за отзыв")
+    }else {
+        alert("дополните отзыв, пожалуйста")
+        var name = prompt("Напишите отзыв ")
+    }
+    }
+    
+    
+    
+
+    
